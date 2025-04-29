f20fd5f06b5cbe9df4a2bf03f814014d47c5e21b
--global <boldrainzz@gmail.com>
2025-04-29 16:46:35 +0800

    merge C4 into B1

--- changes_1.py
+++ changes_1.py
@@ -1,9 +1,3 @@
 for i in range(100):
     print(i)
-<<<<<<< HEAD
 print('B1')
-=======
-
-for i in range(50):
-    print(i)
->>>>>>> C4
--- changes_2.py
+++ changes_2.py
@@ -1,6 +1,2 @@
 print('hello, world!')
-<<<<<<< HEAD
 print('hello, B1')
-=======
-print('world, hello!')
->>>>>>> C4
